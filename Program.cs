d793ecfd19e42f10a3ad49ad226f133f253d6651
Eric Polino <aluink@gmail.com>
2024-08-02 08:26:51 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reversi {
+  class Program {
+    static void Main(string[] args) {
+      // Board b = Board.NewGame();
+      // b.Print();
+
+      //0x0000001008000000UL,
+      //0x0000000810000000UL
+
+      Masks.GenerateNeighborMasks();
+    }
+  }
+}
